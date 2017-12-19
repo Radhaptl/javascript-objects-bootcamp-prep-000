ec177ecc2a7460376173be8eab6bb7df023f55e8
Radha Patel <radhapatel99@gmail.com>
2017-12-19 03:42:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,7 @@
 var playlist = { camilaCabello: 'Havana',
   taylorSwift: 'Ready for it?'};
+
+function updatePlaylist(playlist, artistName, SongTitle) {
+  return Object.assign({}, playlist,{ [artistName]: SongTitle} )
+}
+
